14ad36ae96efedb37966f948bcc767e9dbfbcd65
acronicsM <91975262+acronicsM@users.noreply.github.com>
2023-11-28 16:42:42 +0300

    install sqlalchemy

--- deliveryapi/drivers/views.py
+++ deliveryapi/drivers/views.py
@@ -2,11 +2,9 @@ from fastapi import APIRouter
 
 from .models import FAKE_DRIVERS
 
-
-router = APIRouter(prefix='/drivers', tags=['drivers'])
+router = APIRouter(prefix='/drivers', tags=['Drivers'])
 
 
 @router.get("/")
-def get_orders():
+def get_drivers():
     return [{'id': id, 'name': value['name']} for id, value in FAKE_DRIVERS.items()]
-
--- deliveryapi/orders/views.py
+++ deliveryapi/orders/views.py
@@ -1,37 +1,28 @@
 from fastapi import APIRouter
 
-from deliveryapi.db import db
+from .models import FAKE_ORDERS, FAKE_ITEMS
 
+router = APIRouter(prefix="/orders", tags=["Orders"])
 
-router = APIRouter(prefix='/{driver_id}/orders')
 
 @router.get("/")
-def get_orders(driver_id: int):
-    orders = []
-    if driver_id in db:
-        orders = [{'order_id': order,
-                   'number': db[driver_id][order]['id'],
-                   'buyer': db[driver_id][order]['buyer'],
-                   'telephone': db[driver_id][order]['telephone'],
-                   'address': db[driver_id][order]['address'],
-                   'comment': db[driver_id][order]['comment'],
-                   } for order in db[driver_id]]
-
-    return orders
+def get_orders(driver_id: int = None):
+    if driver_id:
+        return {k: v for k, v in FAKE_ORDERS.items() if v["driver"] == driver_id}
+    return FAKE_ORDERS
 
 
 @router.get("/{order_id}")
-def get_order(driver_id: int, order_id: str):
-    order = dict()
+def get_order(order_id: str):
+    if order_id in FAKE_ORDERS:
+        return FAKE_ORDERS[order_id]
 
-    if driver_id in db and order_id in db[driver_id]:
-        _order = db[driver_id][order_id]
-        order = {'order_id': order_id,
-                 'number': _order['id'],
-                 'buyer': _order['buyer'],
-                 'telephone': _order['telephone'],
-                 'address': _order['address'],
-                 'comment': _order['comment'],
-                 }
+    return dict()
 
-    return order
+
+@router.get("/{order_id}/items")
+def get_items(order_id: str):
+    if order_id in FAKE_ITEMS:
+        return FAKE_ITEMS[order_id]
+
+    return dict()
