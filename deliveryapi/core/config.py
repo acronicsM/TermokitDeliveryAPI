0f16b7601fd12906c4be8c4ac469ed2f0d05b917
acronicsM <91975262+acronicsM@users.noreply.github.com>
2023-11-28 16:46:13 +0300

    install pydantic-settings

--- deliveryapi/core/config.py
+++ deliveryapi/core/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     app_name: str = "Awesome API"
-    db_url = ''
-    model_config = SettingsConfigDict(env_file="../.env")
+    db_url = ""
+    model_config = SettingsConfigDict(env_file="../../.env")
 
     pass
