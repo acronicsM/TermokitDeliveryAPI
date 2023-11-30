f4e46418da1c3af3cdb673502f308f558ef30cbe
acronicsM <91975262+acronicsM@users.noreply.github.com>
2023-11-30 11:43:53 +0300

    Добавил связь между drivers и order

--- deliveryapi/core/models/orders.py
+++ deliveryapi/core/models/orders.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Integer, TEXT, BOOLEAN
+from sqlalchemy import String, Integer, TEXT, BOOLEAN, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
@@ -8,10 +8,13 @@ class Order(Base):
     __tablename__ = "orders"
     id: Mapped[str] = mapped_column(primary_key=True)
     orde_1c_number: Mapped[str] = mapped_column(String(11))
-    id_search: Mapped[int] = mapped_column(Integer)
+    id_search: Mapped[int]
     buyer: Mapped[str] = mapped_column(String(250))
     telephone: Mapped[str] = mapped_column(String(12))
     address: Mapped[str] = mapped_column(TEXT)
     comment: Mapped[str] = mapped_column(TEXT, nullable=True)
-    driver_id: Mapped[int] = mapped_column(Integer)
-    delivered: Mapped[bool] = mapped_column(BOOLEAN)
+    delivered: Mapped[bool]
+
+    driver_id: Mapped[int] = mapped_column(
+        ForeignKey("drivers.id"),
+    )
