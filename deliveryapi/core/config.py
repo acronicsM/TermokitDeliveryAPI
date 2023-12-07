c529ea60f5dfed27d6a636b8cacef64b7fb91920
acronicsM <91975262+acronicsM@users.noreply.github.com>
2023-12-07 15:01:34 +0300

    Исправил путь к бд

--- deliveryapi/core/config.py
+++ deliveryapi/core/config.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 from pydantic_settings import BaseSettings
 
 
-BASE_DIR = Path(__file__).parent.parent
+BASE_DIR = Path(__file__).parent.parent.parent
 DB_PATH = BASE_DIR / "db.sqlite3"
 
 
