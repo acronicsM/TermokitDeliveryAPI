f5d1133092112f8a752335cc7b117b2c30ebc083
acronicsM <91975262+acronicsM@users.noreply.github.com>
2023-12-11 12:26:29 +0300

    Добавил таблицу пользователей

--- deliveryapi/core/models/__init__.py
+++ deliveryapi/core/models/__init__.py
@@ -1,14 +1,8 @@
-__all__ = (
-    "Base",
-    "DatabaseHelper",
-    "db_helper",
-    "Driver",
-    "Order",
-    "Item",
-)
+__all__ = ("Base", "DatabaseHelper", "db_helper", "Driver", "Order", "Item", "User")
 
 from .base import Base
 from .db_helper import DatabaseHelper, db_helper
 from .drivers import Driver
 from .orders import Order
 from .items import Item
+from .users import User
---
+++ deliveryapi/core/models/users.py
@@ -0,0 +1,12 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50))
+    password: Mapped[str]
+    is_active: Mapped[bool]
